f25655963b685b15460a47e976db3313dd3b56c1
saltyposaune <ewalkertbone@gmail.com>
2021-09-04 16:45:21 -0600

    finished category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -37,6 +37,23 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({message: 'No category found with this id'});
+      return;
+    } res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err);
+});
+
+
+
+router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name
+  })
   .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
     console.log(err)
@@ -44,16 +61,40 @@ router.get('/:id', (req, res) => {
   })
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
-
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.body.id // or is it params...?
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({message: 'No category found with this id'});
+      return;
+    } res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err);
+});
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.desttroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({message: 'No category found with this id'});
+      return;
+    } res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err);
+});
 });
 
 module.exports = router;
